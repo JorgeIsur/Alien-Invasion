5e41fe71d47ad64692d90a3e95ab4720242ec68e
JorgeIsur <ramirezisur@gmail.com>
2024-01-16 16:40:57 -0600

    Se logró que la nave dispare proyectiles

--- alien_invasion.py
+++ alien_invasion.py
@@ -9,6 +9,9 @@ from ship import Ship
 # importar funciones del juego
 import game_functions as gf
 
+# importar agrupamiento
+from pygame.sprite import Group
+
 
 def run_game():
     # inicializar el juego, configuraciones y crear un objeto de pantalla
@@ -19,15 +22,19 @@ def run_game():
 
     # instanciar la nave
     ship = Ship(ai_settings,screen)
+    # crear un grupo para almacenar balas en el
+    bullets = Group()
 
     # inicializar el ciclo principal para el juego
     while True:
         # manejador de eventos
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         # Actualizar posición de la nave
         ship.update()
+        # Actualizar posición de balas
+        bullets.update()
         # actualizador de pantalla
-        gf.update_screen(ai_settings,screen,ship)
+        gf.update_screen(ai_settings,screen,ship, bullets)
 
 
 run_game()
--- game_functions.py
+++ game_functions.py
@@ -2,20 +2,23 @@ import sys
 
 import pygame
 
+from bullet import Bullet
 
-def check_events(ship):
+
+def check_events(ai_settings, screen, ship, bullets):
     """Responde a entradas de teclado y eventos del mouse"""
     # esperar eventos de teclado y mouse
     for event in pygame.event.get():
+        #print(event.type)
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-            check_keydown_events(event, ship)
+            check_keydown_events(event, ai_settings, screen, ship, bullets)
         elif event.type == pygame.KEYUP:
             check_keyup_events(event, ship)
 
 
-def check_keydown_events(event, ship):
+def check_keydown_events(event, ai_settings, screen, ship, bullets):
     """Manejar el presionado de teclas"""
     if event.key == pygame.K_RIGHT:
         # mover la nave a la derecha
@@ -23,6 +26,11 @@ def check_keydown_events(event, ship):
     if event.key == pygame.K_LEFT:
         # mover la nave a la izquierda
         ship.moving_left = True
+    elif event.key == pygame.K_SPACE:
+        #print("Espacio")
+        # Crear una nueva bala y añadirla al grupo de balas
+        new_bullet = Bullet(ai_settings, screen, ship)
+        bullets.add(new_bullet)
 
 
 def check_keyup_events(event, ship):
@@ -33,10 +41,13 @@ def check_keyup_events(event, ship):
         ship.moving_left = False
 
 
-def update_screen(ai_settings, screen, ship):
+def update_screen(ai_settings, screen, ship, bullets):
     """Actualiza imagenes en la pantalla"""
     # redibujar la pantalla en cada iteracion del ciclo
     screen.fill(ai_settings.bg_color)
+    # REDIBUJAR TODAS LAS BALAS DETRAS DE LA NAVE Y ALIENS
+    for bullet in bullets.sprites():
+        bullet.draw_bullet()
     ship.blitme()
 
     # mostrar la pantalla
